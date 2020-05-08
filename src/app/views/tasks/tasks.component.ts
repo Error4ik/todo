14f30b24e9fa38c46db557a82e7f9a2b6e771a46
Error <fearerror@yandex.ru>
2020-05-09 00:36:49 +0300

    Added the novalue class for optional fields.

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from '../../interfaces/task';
 import {DataHandlerService} from '../../services/data-handler.service';
 
@@ -11,9 +11,14 @@ export class TasksComponent implements OnInit {
 
   tasks: Task[];
 
-  constructor(private dataHandlerService: DataHandlerService) { }
+  constructor(private dataHandlerService: DataHandlerService) {
+  }
 
   ngOnInit() {
     this.dataHandlerService.tasksSubject.subscribe(tasks => this.tasks = tasks);
+  }
+
+  toggleTaskCompleted(task: Task) {
+    task.completed = !task.completed;
   }
 }
