05d0f6c3722a552b9962b08609881fe4de02dec6
Error <fearerror@yandex.ru>
2020-05-09 00:18:00 +0300

    Added the active class for the selected category.

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -10,6 +10,7 @@ import {Category} from '../../interfaces/category';
 export class CategoriesComponent implements OnInit {
 
   categories: Category[];
+  selectedCategory: Category;
 
   constructor(private dataHandlerService: DataHandlerService) {
   }
@@ -19,6 +20,7 @@ export class CategoriesComponent implements OnInit {
   }
 
   showTasksByCategory(category: Category) {
+    this.selectedCategory = category;
     this.dataHandlerService.fillTasksByCategory(category);
   }
 }
